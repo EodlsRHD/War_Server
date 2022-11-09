7eb17272b0e6924c86aaa72d13c9220c40c99edb
EodlsRHD <101389330+EodlsRHD@users.noreply.github.com>
2022-11-09 17:11:33 +0900

    Winsock RESETfirst

--- MainServer.cpp
+++ MainServer.cpp
@@ -1,10 +1,25 @@
 #include <stdio.h>
 #include <iostream>
+#include <thread>
+
+#include <WinSock2.h>
+#pragma comment (lib , "ws2_32.lib")
 
 using namespace std;
 
+string serverIP = "serverip";
+int acceptPort = 0000;
+SOCKET acceptSocket;
+
 int main(void){
-	cout << "HELLO";
-	// VS COMMIT TEST
-	// VSCODE COMMIT TEST
+
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0){ return 0; }
+
+	acceptSocket = socket(AF_INET, SOCK_STREAM, 0);
+	if (acceptSocket == INVALID_SOCKET) { cout << "서버 생성 실패"; }
+
+	sockaddr addr = { 0 };
+		
+	
 }
