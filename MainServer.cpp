0886799b8f02c3b61591031a198705a965765693
EodlsRHD <101389330+EodlsRHD@users.noreply.github.com>
2022-11-09 17:01:42 +0900

    commit

--- MainServer.cpp
+++ MainServer.cpp
@@ -6,7 +6,4 @@ using namespace std;
 int main(void){
 	cout << "HELLO";
 	// VS COMMIT TEST
->>>>>>>>> Temporary merge branch 2
->>>>>>>>> Temporary merge branch 2
->>>>>>>>> Temporary merge branch 2
 }
