609170c154d25503f196a8021ac452d5a50cf186
EodlsRHD <101389330+EodlsRHD@users.noreply.github.com>
2022-11-09 17:11:33 +0900

    first

--- MainServer.cpp
+++ MainServer.cpp
@@ -1,10 +1,21 @@
 #include <stdio.h>
 #include <iostream>
+#include <thread>
+
+#include <WinSock2.h>
+#pragma comment (lib , "ws2_32.lib")
 
 using namespace std;
 
+string serverIP = "serverip";
+int acceptPort = 0000;
+SOCKET acceptSocket;
+
 int main(void){
-	cout << "HELLO";
-	// VS COMMIT TEST
-	// VSCODE COMMIT TEST
+	acceptSocket = socket(AF_INET, SOCK_STREAM, 0);
+	if (acceptSocket == INVALID_SOCKET) { cout << "서버 생성 실패"; }
+
+	sockaddr addr = { 0 };
+		
+	
 }
